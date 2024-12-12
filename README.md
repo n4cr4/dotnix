2cd4ccf831eecc08018957c107fb09eae05c537c
n4cr4 <n4cr4.dev@gmail.com>
2024-12-12 20:27:08 +0900

    feat: flake init

---
+++ README.md
@@ -0,0 +1,14 @@
+# dotnix
+
+n4cr4's configurations.
+
+# Installation
+
+## Nix
+
+Use [nix-installer](https://github.com/DeterminateSystems/nix-installer) .
+
+```
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+  sh -s -- install
+```
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Home Manager configuration of n4cr4";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = 
+        { 
+        self,
+        nixpkgs,
+        home-manager,
+        ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."n4cr4" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+                    inherit inputs;
+                };
+
+        modules = [ ./home.nix ];
+      };
+    };
+}
