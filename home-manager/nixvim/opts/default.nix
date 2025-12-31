7741214c939a1ba2b808fdba08e4be37d497970a
n4cr4 <n4cr4.dev@gmail.com>
2026-01-01 02:30:22 +0900

    fix: remove plugins

--- home-manager/nixvim/opts/default.nix
+++ home-manager/nixvim/opts/default.nix
@@ -7,6 +7,26 @@
       maplocalleader = " ";
       autoformat = false;
       snacks_animate = false;
+
+      # Disable default plugins
+      loaded_gzip = 1;
+      loaded_zip = 1;
+      loaded_zipPlugin = 1;
+      loaded_tar = 1;
+      loaded_tarPlugin = 1;
+      loaded_getscript = 1;
+      loaded_getscriptPlugin = 1;
+      loaded_vimball = 1;
+      loaded_vimballPlugin = 1;
+      loaded_2html_plugin = 1;
+      loaded_matchit = 1;
+      loaded_matchparen = 1;
+      loaded_logiPat = 1;
+      loaded_rrhelper = 1;
+      loaded_netrw = 1;
+      loaded_netrwPlugin = 1;
+      loaded_netrwSettings = 1;
+      loaded_netrwFileHandlers = 1;
     };
 
     # General options
@@ -34,6 +54,8 @@
       wrap = true;
       conceallevel = 0;
       cursorline = true;
+      laststatus = 3; # Global statusline
+      cmdheight = 0; # Hide command line when not in use
 
       # Behavior
       mouse = "";
